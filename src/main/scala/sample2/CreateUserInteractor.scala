231d7bb23c96e6853e0237a3c1e695e684eabc80
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-05-20 23:29:23 +0900

    commit

--- src/main/scala/sample2/CreateUserInteractor.scala
+++ src/main/scala/sample2/CreateUserInteractor.scala
@@ -11,10 +11,13 @@ class CreateUserInteractor(
   override def handle(input: CreateUserInput): CreateUserOutput = {
     val newUser: User[NotValid] = User.create(input.name)
 
+    // userRepository.insert(newUser) <--- ここでinsertしようとすると Type mismatch でエラーになる
+
     newUser.validate(userNameConflictValidator) match {
       case Left(_) => UserNameConflict(newUser.name)
       case Right(validUser) =>
         userRepository.insert(validUser)
+        CreateUserOutput.Success2(validUser)
     }
   }
 }
--- src/main/scala/usecase/create/CreateUserOutput.scala
+++ src/main/scala/usecase/create/CreateUserOutput.scala
@@ -1,11 +1,12 @@
 package usecase.create
 
 import ddd.Output
-import sample1.User
+import sample2.Valid
 import user.UserName
 
 sealed abstract class CreateUserOutput extends Output
 object CreateUserOutput {
-  final case class Success(newUser: User)                       extends CreateUserOutput
+  final case class Success(newUser: sample1.User)               extends CreateUserOutput
+  final case class Success2(newUser: sample2.User[Valid])       extends CreateUserOutput // 面倒なので一緒にしちゃった
   final case class UserNameConflict(conflictUserName: UserName) extends CreateUserOutput
 }
